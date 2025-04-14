3ba2f60a6608507bb1a0c36a37236b01f5972838
Alinson López <alinsonl11@gmail.com>
2025-04-14 22:34:56 +0000

    implementing auto user deleting

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,13 +1,37 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request } from '@playwright/test';
 import { RegisterPage } from '../pages/register.page';
+import { before } from 'node:test';
 
+const email: string = 'alinson.lopez@example.com';
+const apiUrl: string = 'https://automation-portal-bootcamp.vercel.app/api/user';
 
 test('Registering Passed', async ({ page }) => {
     const registerPage = new RegisterPage(page);
     await registerPage.goto();
     await registerPage.fillFirstName('Alinson');
     await registerPage.fillLastName('Lopez');
-    await registerPage.fillEmail('alinson.lopez@example.com');
+    await registerPage.fillEmail(email);
     await registerPage.fillPassword('password123');
     await registerPage.clickRegisterButton();
+})
+
+test.afterEach(async ({ page }) => {
+    const requestContext = await request.newContext();
+    const getResponse = await requestContext.get(`${apiUrl}?email=${email}`);
+    const userData = await getResponse.json();
+    console.log(userData);
+
+    if (userData && userData.id) {
+        console.log(`deleting user... ${userData.name}`);
+        const deleteResponse = await requestContext.delete(`${apiUrl}/${userData.id}`, {
+            headers: {
+                'Authorization': 'Bearer mi-token-super-secreto'
+            }
+        });
+        if (deleteResponse.ok()) {
+            console.log('User deleted successfully');
+        } else {
+            console.error('Failed to delete user');
+        }
+    }
 })
